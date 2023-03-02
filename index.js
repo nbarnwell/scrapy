4b1f6fc09b1c887540cb0d0986ebfdd3d7eec826
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2023-03-02 19:04:16 +0000

    Returning price for first product

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import { JSDOM } from 'jsdom';
 import jQuery from 'jQuery';
 import fetch from 'node-fetch';
 
-const response = await fetch('https://www.ryman.co.uk/really-useful-box-9-litre-pack-of-9-clear');
-const body = await response.text();
+async function getJQuery(url) {
+    const response = await fetch(url);
+    const body = await response.text();
+    const dom = new JSDOM(body);
+    return Promise.resolve(new jQuery(dom.window));
+}
 
-console.log(body);
-
-const dom = new JSDOM(body);
-const $ = new jQuery(dom.window);
-
-const price = $('span.price').text();
+const rymanReallyUsefulBoxesPackOf9Page = await getJQuery('https://www.ryman.co.uk/really-useful-box-9-litre-pack-of-9-clear');
+const price = rymanReallyUsefulBoxesPackOf9Page('div.product-info-wrapper span[data-price-type=finalPrice]').attr('data-price-amount');
 console.log(price);
