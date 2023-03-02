1f08204b0f8219cdbd704407b0b73f45f422806c
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2023-03-02 18:48:50 +0000

    Calling ryman website

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import { JSDOM } from 'jsdom';
 import jQuery from 'jQuery';
+import fetch from 'node-fetch';
 
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+const response = await fetch('https://www.ryman.co.uk/really-useful-box-9-litre-pack-of-9-clear');
+const body = await response.text();
+
+console.log(body);
+
+const dom = new JSDOM(body);
 const $ = new jQuery(dom.window);
 
-const p = $('p').text();
-console.log(p);
+const price = $('span.price').text();
+console.log(price);
